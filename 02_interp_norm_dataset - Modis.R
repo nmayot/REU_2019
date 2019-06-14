920854ec18ad3a11e39a98098d6404d3d7cb50e6
Mariah Ricci <51246950+mricci1@users.noreply.github.com>
2019-06-14 10:29:45 -0400

    MODIS script
    Changed filenames and time periods

---
+++ 02_interp_norm_dataset - Modis.R
@@ -0,0 +1,54 @@
+### Interp. and Norm. before Clustering analysis ---------------------------------
+#
+#
+# --- Open dataset --------------------------------------------------
+#
+# dataset == List of 6
+#   $ CHL    : num [1:97920, 1:46] NaN NaN NaN NaN NaN NaN NaN NaN NaN NaN ...
+#   $ LON    : num [1:97920] -5.96 -5.87 -5.79 -5.71 -5.62 ...
+#   $ LAT    : num [1:97920] 46 46 46 46 46 ...
+#   $ mask   : num [1:510, 1:192] 0 0 0 0 0 0 0 0 0 0 ...
+#   $ lon_map: num [1:510(1d)] -5.96 -5.87 -5.79 -5.71 -5.62 ...
+#   $ lat_map: num [1:192(1d)] 46 45.9 45.8 45.7 45.6 ...
+
+rm(list=ls()) # clear all variable
+
+#--- Read dataset ----
+# folder <- "C:/Users/nmayot/Documents/PostDoc/data/satellite/SeaWiFS/" # folder with data
+folder <- "C:/Users/mricc/OneDrive/Documents/2019 REU/DATA/SeaWiFS/" # folder with data
+filename <- "SeaWiFS_climato_1997_2007_8D_CHL.rdata" # Name of the dataset
+
+load(file = paste(folder,filename,sep = "")) # Load dataset
+
+CHL <- dataset$CHL # climatological dataset (row = pixel, column = weeks)
+nweeks <- dim(CHL)[2] # number of weeks (= number of columns)
+
+
+# --- Interpollation and Normalization ----------------------------------------------
+#
+#
+# Linear interpolation to remove NA, and divide each time series by its maximal value 
+
+CHL_norm <- array(dim = dim(CHL)) # same dimension as the non-normalized dataset
+CHL_max <- array(dim = c(dim(CHL)[1],1)) # maximal value for each time series (one timeserie per pixel)
+
+# loop over each pixel (= timeserie)
+for (i in 1:nrow(CHL)) {
+
+  r <- CHL[i,] # row timeserie
+  n <- which(!is.na(r)) # locations of non-NAs
+  diff_n <- n[2:length(n)] - n[1:length(n)-1] # number of weeks inbetween non-NAs
+  
+  if (length(n) >= round(nweeks/2) & length(which(diff_n >= 5)) == 0) { # at least a half weeks of data with a data point every 4 weeks 
+    r_interp <- approx(1:nweeks,r,xout=1:nweeks,rule=2)$y # linearly interpolate (extrapolation repeat the closest known values)
+    CHL_max[i] <- max(r_interp) # save max value of the timeserie
+    CHL_norm[i,] <- r_interp/CHL_max[i] # normalization (divide by maximal value)
+  }
+}
+
+#--- Save dataset ----
+dataset$CHL_norm <- CHL_norm # latitude of the MedSea map
+dataset$CHL_max <- CHL_max # latitude of the MedSea map
+dataset$gp <- which(!is.na(CHL_norm[,1])) # pixels (timeseries) without NA to be clusterized (gp = good points)
+save(dataset, file = paste(folder,"SeaWiFS_climato_1997_2007_8D_CHL_CHLnorm.rdata",sep="")) # save rdata into the same folder
+
---
+++ practice.R
@@ -0,0 +1,86 @@
+# Prepare the dataset, with the climatological Chla values, for the kmeans analysis
+#
+#
+# --- Create dataset --------------------------------------------------
+#
+# dataset == List of 3
+#   $ CHL: num [1:..., 1:46]
+#   $ lat : num [1:...]
+#   $ lon : num [1:...]
+
+rm(list=ls()) # clear all variable
+library("ncdf4") # package to open NetCDF
+
+# folder <- "C:/Users/nmayot/Documents/PostDoc/data/satellite/SeaWiFS/" # folder with data
+folder <- "C:/Users/mricc/OneDrive/Documents/2019 REU/DATA/SeaWiFS/" # folder with data
+filenames <- list.files(path = folder) # all filenames into this folder
+nweeks <- 46 # there are 46 weeks of 8-day in one year
+jdays <- seq(1,366,8) # julian day of the first day of each 8-day week (1, 9, 17..., 361)
+years <- 1997:2007 # years of the climato
+load(paste(folder,"mask_SW.Rdata",sep="")) # mask of the MedSea for SeaWiFS data
+
+chl_clim <- c() # to save the CHL data
+
+# open the data, for each week and year
+for (j in jdays) {  
+  
+  chl_week <- c() # to save the CHL data of each year before average
+  
+  for (y in years) {
+    
+    fname <- filenames[substr(filenames,2,8) == paste(toString(y),sprintf("%03d",j),sep="")] # search filename by year and date
+    
+    if (length(fname) != 0) {
+      filename <- list.files(path = folder, pattern = fname, full.names = T)  # search filename in directory
+      nc <- nc_open(filename)
+      
+      # open latitudes and longitudes data only one time
+      if (!exists("lon")) {
+        lat <- ncvar_get(nc, "lat")
+        lon <- ncvar_get(nc, "lon")
+        
+        # search in the NetCDF file the locations of pixel in a square around the MedSea 
+        startlon <- min(which(lon >= -6 & lon <= 36.5))
+        countlon <- abs(min(which(lon >= -6 & lon <= 36.5)) - max(which(lon >= -6 & lon <= 36.5))) + 1
+        startlat <- min(which(lat >= 30 & lat <= 46))
+        countlat <- abs(min(which(lat >= 30 & lat <= 46)) - max(which(lat >= 30 & lat <= 46))) + 1
+        
+        # lon and lat vectors for the map
+        lon <- lon[lon >= -6 & lon <= 36.5]
+        lat <- lat[lat >= 30 & lat <= 46]
+        
+        # lon and lat vectors for each pixel
+        LON <- matrix(lon, length(lon), length(lat))
+        LAT <- matrix(lat, length(lon), length(lat), byrow = TRUE)
+        LON <- as.vector(LON)
+        LAT <- as.vector(LAT)
+      }
+      
+      # Subset the NetCDF file at the locations wanted
+      chla <- ncvar_get(nc, "chlor_a", start = c(startlon,startlat), count = c(countlon,countlat))
+      chla[mask_SW == 0] <- NA # pixel oustside MedSea removed
+      chla <- as.vector(chla)
+      
+      # save into the matrix before weekly average
+      chl_week <- cbind(chl_week, chla)
+      
+      # close NetCDF file
+      nc_close(nc)
+    }
+    
+  }
+  chl_week <- rowMeans(chl_week, na.rm = TRUE) # weekly climatology
+  chl_clim <- cbind(chl_clim, chl_week) # save into matrix (row = pixel, column = week)
+}
+dimnames(chl_clim) <- c() # remove dimension names (e.g., colnames)
+
+#----- Build the dataset and save it
+dataset <- c()
+dataset$CHL <- chl_clim # rows = pixels, column = 8-day weeks (x46)
+dataset$LON <- LON # rows = longitude of each pixel
+dataset$LAT <- LAT # rows = latitude of each pixel
+dataset$mask <- mask_SW # mask of the MedSea used
+dataset$lon_map <- lon # longitude of the MedSea map
+dataset$lat_map <- lat # latitude of the MedSea map
+save(dataset, file = paste(folder,"SeaWiFS_climato_1997_2007_8D_CHL.rdata",sep="")) # save rdata into the same folder
+
